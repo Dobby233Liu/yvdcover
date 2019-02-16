cfc8c12ad4c5d3961c6f68557e4022cdedbfc62e
Liu-Wen-Yuan <15816141883@163.com>
2019-02-16 11:56:10 +0800

    Add files via upload

--- traviswine.sh
+++ traviswine.sh
@@ -5,3 +5,4 @@ sudo apt-key add winehq.key
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ trusty main'
 sudo apt update
 sudo apt install --install-recommends winehq-devel
+sudo apt install wine-mono

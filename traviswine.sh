b8e1e7730f4c57ac7433fd22fb1506f6b2449055
Liu-Wen-Yuan <15816141883@163.com>
2019-02-16 12:25:08 +0800

    .

--- traviswine.sh
+++ traviswine.sh
@@ -5,4 +5,9 @@ sudo apt-key add winehq.key
 sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ trusty main'
 sudo apt update
 sudo apt install --install-recommends winehq-devel
-sudo apt install wine-mono
+# winecfg
+# sudo apt install wine-mono
+# sudo npm install -g winresourcer
+sudo wget http://dl.winehq.org/wine/wine-mono/4.7.5/wine-mono-4.7.5.msi -o /usr/share/wine/mono/wine-mono-4.7.5.msi
+sudo apt-get install winetricks
+winetricks

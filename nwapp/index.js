932020ad257840903d9aafbd42055f9bb7a5a18e
LiuWenYuan <15816141883@163.com>
2020-04-11 09:50:55 +0800

    fixup!!

--- nwapp/index.js
+++ nwapp/index.js
@@ -20,9 +20,8 @@ window.parseVideo = function (url) {
 
 // by myself
 window.hyperId = function(w, ytu){
-	if(!(ytu.toLowerCase().indexOf('bv')>-1)) ytu = ytu.toLowerCase();
     if (ytu.indexOf('youtu') > -1 || ytu.indexOf('vimeo') > -1 || ytu.indexOf('dailymotion') > -1 || ytu.indexOf('bilibili') > -1||ytu.indexOf('b23') > -1) return w.parseVideo(ytu);
-    if(ytu.indexOf('av')>-1||ytu.indexOf('cv')>-1) return {id: ytu, type: "bilibili"};
+    if(ytu.indexOf('av')>-1||ytu.indexOf('cv')>-1||ytu.indexOf('AV')>-1||ytu.indexOf('CV')>-1) return {id: ytu, type: "bilibili"};
 	if(ytu.indexOf('bv')>-1||ytu.indexOf('BV')>-1) return {id: ytu, type: "bilibili-bv"};
     return {id: ytu, type: "youtube"};
 }
